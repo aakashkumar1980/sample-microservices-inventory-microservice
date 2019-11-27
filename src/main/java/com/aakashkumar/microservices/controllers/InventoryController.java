a136bfa92ac5156efab4f299dddfcbb06f1aee59
Aakash Kumar <aakash.kumar1980@yahoo.com>
2019-11-27 07:59:51 +0000

    temp

--- src/main/java/com/aakashkumar/microservices/controllers/InventoryController.java
+++ src/main/java/com/aakashkumar/microservices/controllers/InventoryController.java
@@ -3,6 +3,7 @@ package com.aakashkumar.microservices.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class InventoryController {
 	@Autowired ModelMapperUtils modelMapperUtils;
 	@Autowired InventoryService inventoryService;
 
+	@CrossOrigin
 	@RequestMapping("/")
 	public List<InventoryDto> getInventory(@RequestParam List<Long> productIds) {
 		return modelMapperUtils.mapEntityToDto(inventoryService.getInventory(productIds), InventoryDto.class);
